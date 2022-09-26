59c93a44e0277332048a8da3aa5d6305845c478d
ffriedl89 <ffriedl89@gmail.com>
2022-09-26 21:39:33 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { styles } from './App.css';
+
+function App() {
+  return (
+    <div className={styles}>
+      <header>
+        <h1>Vanilla Extract</h1>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
